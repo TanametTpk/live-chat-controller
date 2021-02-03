f02c1ae6988b8923c83d8deee8d2f43a3da4458d
tanamettpk <tanamet.tpk@gmail.com>
2021-02-03 09:05:50 +0700

    + switch command adapter by config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import fs from 'fs'
 import LiveChatController from './controllers/LiveChatController'
 import ILiveChatSubscriber from './services/interfaces/ILiveChatSubscriber'
 import ILiveChatPublisher from './services/interfaces/ILiveChatPublisher'
-import YoutubeApiLiveChatPublisher from './services/YoutubeApiLiveChatPublisher'
 import ScrapingLiveChatPublisher from './services/ScrapingLiveChatPublisher'
 import ICommandSubscriber from './services/interfaces/ICommandSubscriber'
 import LocalIOController from './controllers/LocalIOController'
@@ -10,15 +8,21 @@ import ICommandPublisher from './services/interfaces/ICommandPublisher'
 import LocalIOPublisher from './services/LocalIOPublisher'
 import RobotJSIOController from './services/RobotJSIOController'
 import LiveChatAdapter from './services/LiveChatAdapter'
+import { loadCommandConfig, loadLiveChatConfig } from './utils/loadConfig'
+import AbstractLiveChatAdapter from './services/abstracts/AbstractLiveChatAdapter'
+import LiveChatCustomCommandAdapter from './services/LiveChatCustomCommandAdapter'
 
-const raw_configs: string = fs.readFileSync('./config.json').toString()
-const configs = JSON.parse(raw_configs)
+const liveChatConfig = loadLiveChatConfig('./config.json')
+const commandsConfig = loadCommandConfig('./commands.json')
 
 const ioController: RobotJSIOController = new RobotJSIOController()
 
 const chatController: ILiveChatSubscriber = new LiveChatController(ioController, ioController)
-const customChatCommandAdapter: LiveChatAdapter = new LiveChatAdapter(chatController, [])
-const chatPublisher: ILiveChatPublisher = new ScrapingLiveChatPublisher(configs)
+const chatPublisher: ILiveChatPublisher = new ScrapingLiveChatPublisher(liveChatConfig)
+
+let customChatCommandAdapter: AbstractLiveChatAdapter
+if (commandsConfig.useOnlyDefined) customChatCommandAdapter = new LiveChatCustomCommandAdapter(chatController, commandsConfig.commands)
+else customChatCommandAdapter = new LiveChatAdapter(chatController, commandsConfig.commands)
 
 const localController: ICommandSubscriber = new LocalIOController(ioController)
 const ioPublisher: ICommandPublisher = new LocalIOPublisher()
