01a662506176fc8b27e0c5ca71cab8974a298f14
tanamettpk <tanamet.tpk@gmail.com>
2021-02-03 09:52:51 +0700

    + Mock class

---
+++ src/test/adapter.spec.ts
@@ -0,0 +1,24 @@
+import MockChatController from "../controllers/MockChatController"
+import AbstractLiveChatAdapter from "../services/abstracts/AbstractLiveChatAdapter"
+import ILiveChatPublisher from "../services/interfaces/ILiveChatPublisher"
+import ILiveChatSubscriber from "../services/interfaces/ILiveChatSubscriber"
+import LiveChatAdapter from "../services/LiveChatAdapter"
+import LiveChatCustomCommandAdapter from "../services/LiveChatCustomCommandAdapter"
+import MockChatPublisher from "../services/MockChatPublisher"
+import { loadCommandConfig } from "../utils/loadConfig"
+
+const commandsConfig = loadCommandConfig('./commands.json')
+
+const mockController: ILiveChatSubscriber = new MockChatController()
+const mockPublisher: ILiveChatPublisher = new MockChatPublisher([
+    "ขวาค้าง",
+    "right holds",
+    "rh"
+])
+
+let customChatCommandAdapter: AbstractLiveChatAdapter
+if (commandsConfig.useOnlyDefined) customChatCommandAdapter = new LiveChatCustomCommandAdapter(mockController, commandsConfig.commands)
+else customChatCommandAdapter = new LiveChatAdapter(mockController, commandsConfig.commands)
+
+mockPublisher.register(customChatCommandAdapter)
+mockPublisher.start()
